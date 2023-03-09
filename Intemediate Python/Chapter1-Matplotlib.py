940c822f5e75193e9efb051bf0bf6bf100d4b248
lptrinh <125241615+lptrinh@users.noreply.github.com>
2023-03-09 15:06:52 +0700

    Update Chapter1-Matplotlib.py

--- Intemediate Python/Chapter1-Matplotlib.py
+++ Intemediate Python/Chapter1-Matplotlib.py
@@ -112,102 +112,88 @@ You're a professor in Data Analytics with Python, and you want to visually asses
 
 ##Customization
 
-#Labels
+###Labels
 # Basic scatter plot, log scale
 plt.scatter(gdp_cap, life_exp)
 plt.xscale('log') 
-
 # Strings
 xlab = 'GDP per Capita [in USD]'
 ylab = 'Life Expectancy [in years]'
 title = 'World Development in 2007'
-
 # Add axis labels
-
 plt.xlabel(xlab)
 plt.ylabel(ylab)
 # Add title
 plt.title(title)
-
 # After customizing, display the plot
 plt.show()
 
 
-
-#Ticks
+##Ticks
 # Scatter plot
 plt.scatter(gdp_cap, life_exp)
-
 # Previous customizations
 plt.xscale('log') 
 plt.xlabel('GDP per Capita [in USD]')
 plt.ylabel('Life Expectancy [in years]')
 plt.title('World Development in 2007')
-
 # Definition of tick_val and tick_lab
 tick_val = [1000,10000,100000]
 tick_lab = ['1k','10k','100k']
-
 # Adapt the ticks on the x-axis
 plt.xticks(tick_val,tick_lab)
-
 # After customizing, display the plot
 plt.show()
 
-#Sizes
+
+###Sizes
 # Import numpy as np
 import numpy as np
-
 # Store pop as a numpy array: np_pop
 np_pop=np.array(pop)
-
 # Double np_pop
-
 np_pop = 2*np_pop
 # Update: set s argument to np_pop
 plt.scatter(gdp_cap, life_exp, s=np_pop)
-
 # Previous customizations
 plt.xscale('log') 
 plt.xlabel('GDP per Capita [in USD]')
 plt.ylabel('Life Expectancy [in years]')
 plt.title('World Development in 2007')
 plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
-
 # Display the plot
 plt.show()
 
-# Colors
+
+### Colors
 # Specify c and alpha inside plt.scatter()
 plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2,c=col,alpha=0.8)
-
 # Previous customizations
 plt.xscale('log') 
 plt.xlabel('GDP per Capita [in USD]')
 plt.ylabel('Life Expectancy [in years]')
 plt.title('World Development in 2007')
 plt.xticks([1000,10000,100000], ['1k','10k','100k'])
-
 # Show the plot
 plt.show()
 
-# Additional Customizations
+### Additional Customizations
 # Scatter plot
 plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
-
 # Previous customizations
 plt.xscale('log') 
 plt.xlabel('GDP per Capita [in USD]')
 plt.ylabel('Life Expectancy [in years]')
 plt.title('World Development in 2007')
 plt.xticks([1000,10000,100000], ['1k','10k','100k'])
-
 # Additional customizations
 plt.text(1550, 71, 'India')
 plt.text(5700, 80, 'China')
-
 # Add grid() call
 plt.grid(True) 
-
 # Show the plot
 plt.show()
+
+
+### Interpretation
+>The countries in blue, corresponding to Africa, have both low life expectancy and a low GDP per capita.
