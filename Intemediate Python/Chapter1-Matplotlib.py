52620fb8f0e5cfc43d2a1c5a205b437495f3bcfe
lptrinh <125241615+lptrinh@users.noreply.github.com>
2023-03-09 14:22:38 +0700

    Update Chapter1-Matplotlib.py

--- Intemediate Python/Chapter1-Matplotlib.py
+++ Intemediate Python/Chapter1-Matplotlib.py
@@ -74,6 +74,9 @@ plt.show()
 
 ##Histogram
 
+values = [0, 0.6, 1.4, 1.6, 2.2, 2.5, 2.6, 3.2, 3.5, 3.9, 4.2, 6]
+plt.hist(values, bins=3)
+
 #Build a histogram (1)
 # Create histogram of life_exp data
 plt.hist(life_exp)
